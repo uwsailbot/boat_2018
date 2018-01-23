18096f07435d5aa43d79408017dc9adebdabbf12
Seamus Johnston <sj@trexorobotics.com>
2018-01-23 14:38:10 -0500

    Added anemometer integration to keyboard

--- boat_description/src/joy.py
+++ boat_description/src/joy.py
@@ -6,13 +6,14 @@ import rospy
 import sys, select, termios, tty
 from sensor_msgs.msg import Joy
 from boat_msgs.msg import BoatState
+from std_msgs.msg import Float32
 import time
 
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 
 msg = """
-Reading from the keyboard  and Publishing to Joy!
+Reading from the keyboard and Publishing to Joy!
 ---------------------------
 Moving the rudder:
 a: move rudder left
@@ -25,13 +26,13 @@ Any other key recenters the rudder
 t: tack
 g: cancel tack
 
-To change the angle at which the rudder moves,
-Update the movement factor:
+o: shift wind left 5 degrees
+p: shift wind right 5 degrees
+
+Change rudder limits:
 w: increase + 5 degrees
 x: decrease - 5 degrees
-
-Default: rudder actuates at 45 degrees, limited to 60 max
-
+By default rudder actuates at 45 degrees, limited to 60 max
 
 CTRL-C to quit
 """
@@ -55,6 +56,10 @@ tackBinding={
 		'g':0
 	      }
 
+windBinding={
+		'p':5,
+		'o':-5
+		  }
 
 def getKey():
 	tty.setraw(sys.stdin.fileno())
@@ -63,17 +68,18 @@ def getKey():
 	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 	return key
 
-def writeLine(rudder, tacking, angle, auto):
+def writeLine(rudder, tacking, angle, auto, wind):
 	sys.stdout.write(CURSOR_UP_ONE)
 	sys.stdout.write(ERASE_LINE)
 	sys.stdout.write(CURSOR_UP_ONE)
 	sys.stdout.write(ERASE_LINE)
 	print "Limits: %s, Tacking: %s, AutoMode: %s" % (angle, tacking, auto)
-	print "Rudder: %s" % rudder
+	print "Rudder: %s, Wind: %s" % (rudder, wind)
 
 if __name__=="__main__":
 	settings = termios.tcgetattr(sys.stdin)
-	pub = rospy.Publisher('joy', Joy, queue_size = 1)
+	joy_pub = rospy.Publisher('joy', Joy, queue_size = 1)
+	wind_pub = rospy.Publisher('anemometer', Float32, queue_size = 1)
 	rospy.init_node('keyboard_node')
 	rate = rospy.Rate(100)
 	status = 0
@@ -81,6 +87,7 @@ if __name__=="__main__":
 	auto = 0
 	tack = 0
 	actuating_angle = 45
+	anemometer = 0
 
 	try:
 		print msg
@@ -90,17 +97,17 @@ if __name__=="__main__":
 			# One of the rudder pos keys is hit, update rudder position
 			if key in rudderBinding.keys():
 				rudder_pos = 90 + rudderBinding[key]*actuating_angle
-				writeLine(rudder_pos, tack, actuating_angle, auto)
+				writeLine(rudder_pos, tack, actuating_angle, auto, anemometer)
 
 			# One of the state setting keys hit, update state
 			elif key in autoBinding.keys():
 				auto = autoBinding[key]
-				writeLine(rudder_pos, tack, actuating_angle, auto)
+				writeLine(rudder_pos, tack, actuating_angle, auto, anemometer)
 
 			# One of the tacking keys hit, update tacking state
 			elif key in tackBinding.keys():
 				tack = tackBinding[key]
-				writeLine(rudder_pos, tack, actuating_angle, auto)
+				writeLine(rudder_pos, tack, actuating_angle, auto, anemometer)
 
 			# One of the angle keys hit, update max angle
 			elif key in angleBinding.keys():
@@ -114,7 +121,17 @@ if __name__=="__main__":
 				if (status == 14):
 					print msg
 				status = (status + 1) % 15
-				writeLine(rudder_pos, tack, actuating_angle, auto)
+				writeLine(rudder_pos, tack, actuating_angle, auto, anemometer)
+
+			# One of the wind direction keys was hit
+			elif key in windBinding.keys():
+				anemometer = anemometer + windBinding[key]
+				if anemometer >= 360:
+					anemometer = anemometer - 360
+				elif anemometer < 0:
+					anemometer = anemometer + 360
+				writeLine(rudder_pos, tack, actuating_angle, auto, anemometer)
+
 			else:
 				rudder_pos = 90
 				# Control C quit
@@ -127,7 +144,11 @@ if __name__=="__main__":
 			joy_buttons = [tack,0,(not tack),0,(not auto),auto,0,0,0,0,0]
 			joy.axes = joy_axes
 			joy.buttons = joy_buttons
-			pub.publish(joy)
+			joy_pub.publish(joy)
+
+			wind = Float32()
+			wind.data = anemometer
+			wind_pub.publish(wind)
 			rate.sleep()
 
 	except rospy.ROSInterruptException:
