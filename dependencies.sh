ca7eaf58a2a7abc03561b1c5b124e08211dfddee
Seamus Johnston <sj@trexorobotics.com>
2018-03-09 11:55:10 -0500

    First attempt at Travis CI integration

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+# NOTE: This is a file to install dependancies for travis.  Rosdep currently
+# installs most dependancies, but those that can't be added to the package.xml
+# file must be added here
+sudo apt-get install freetype-py
+
