f9a6d61e32ea05949218dbf3b328572e09fdcb2e
Seamus Johnston <sj@trexorobotics.com>
2018-03-09 12:50:44 -0500

    Fixed dependency issues, and reconfigured travis

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,6 @@
 # NOTE: This is a file to install dependancies for travis.  Rosdep currently
 # installs most dependancies, but those that can't be added to the package.xml
-# file must be added here
-sudo apt-get install freetype-py
+# file must be added here,  to download from apt
+sudo apt-get install python-pip
+pip install freetype-py
 
