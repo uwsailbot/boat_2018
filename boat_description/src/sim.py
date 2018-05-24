b1af3c48345bad527ccf46d17aeecc5510cb8cb8
Cliff <clifflyc@gmail.com>
2018-05-24 13:39:05 -0400

    improved fps when viewing search algo in sim

--- boat_description/src/sim.py
+++ boat_description/src/sim.py
@@ -115,7 +115,75 @@ obstacle_points = PointArray()
 target_point = Waypoint()
 gps_bounding_box = PointArray()
 
+
 # ROS data -- search
+class SearchAreaSection:
+	def __init__(self, center, length, recursive_level, search_area):
+		self.center = center
+		self.length = length
+		self.children = []
+		self.recursive_level = recursive_level
+		self.full= True
+		if recursive_level > 0:
+			self.add_child(Point(center.x+length/4.0, center.y+length/4.0), search_area)
+			self.add_child(Point(center.x+length/4.0, center.y-length/4.0), search_area)
+			self.add_child(Point(center.x-length/4.0, center.y-length/4.0), search_area)
+			self.add_child(Point(center.x-length/4.0, center.y+length/4.0), search_area)
+
+	def add_child(self, child_center, search_area):
+		if self.recursive_level > 1 or (child_center.x-search_area.center.x)**2+(child_center.y-search_area.center.y)**2 < search_area.radius**2:
+			self.children.append(SearchAreaSection(child_center, self.length/2.0, self.recursive_level - 1, search_area))
+
+	def update(self):
+		# recursively do this so we can prune children at all levels
+		if self.recursive_level > 1:
+			# recursively update children
+			for child in self.children:
+				child.update()
+		
+		to_remove = []
+		for child in self.children:
+			if self.full and not child.full:
+				self.full = False
+			# remove leaf nodes who are in fov, remove non-leaf nodes who have no children
+			if child.recursive_level is 0:
+				if point_is_in_fov(child.center):
+					to_remove.append(child)
+			else:
+				if len(child.children) is 0:
+					to_remove.append(child)
+		for child in to_remove:
+			self.children.remove(child)
+
+		# consider oneself full when it has 4 children and all its children are full
+		# full means all of its non-leaf children have 4 children (are complete squares)
+		if self.full:
+			if len(self.children) is not 4:
+				self.full = False
+			else:
+				for child in self.children:
+					if not child.full:
+						self.full = False
+	
+	def draw(self, camera):
+		glBegin(GL_QUADS)
+		self._draw(camera)
+		glEnd()
+
+	def _draw(self, camera):
+		if self.recursive_level is 0 or self.full:
+			glColor4f(self.recursive_level*0.2, self.recursive_level*0.2, self.recursive_level*0.2, 0.3)
+			extent = self.length/2.0 * camera.scale
+			(x, y) = camera.lps_to_screen(self.center.x, self.center.y)
+			glVertex2f(x+extent, y-extent)
+			glVertex2f(x-extent, y-extent)
+			glVertex2f(x-extent, y+extent)
+			glVertex2f(x+extent, y+extent)
+		else:
+			for child in self.children:
+				child._draw(camera)
+	
+
 class SearchArea:
 
 	def __init__(self, center, radius, target):
@@ -124,39 +192,42 @@ class SearchArea:
 		self.target = target
 		self.sections = None
 
-	def setup_coverage(self, resolution=16):
+	def setup_coverage(self, resolution=4):
 		if self.center is None or self.radius is 0 or self.target is None:
 			print("Tried to setup coverage check for search but search area was not completely setup (c:%s r:%s t:%s)" % (self.center, self.radius, self.target))
 			return
 		self.resolution = int(resolution)
-		self.grid_size = float(fov_radius)/self.resolution
 		sections = []
 		sections_per_half_row = int(float(self.radius)/fov_radius)+1
 		self.sections_per_row = sections_per_half_row*2
 
 		for i in range(-sections_per_half_row, sections_per_half_row):
-			section_x = self.center.x + fov_radius*i
+			section_x = self.center.x + fov_radius*i + fov_radius/2.0
 			for j in range(-sections_per_half_row, sections_per_half_row):
-				section_y = self.center.y + fov_radius*j
-				section = []
-				for k in range(0, self.resolution):
-					x = section_x + (k+0.5)*self.grid_size
-					for l in range(0, self.resolution):
-						y = section_y + (l+0.5)*self.grid_size
-						if (self.center.x-x)**2+(self.center.y-y)**2 < self.radius**2:
-							section.append(Point(x,y))
+				section_y = self.center.y + fov_radius*j + fov_radius/2.0
+				# section = []
+				# for k in range(0, self.resolution):
+				# 	x = section_x + (k+0.5)*self.grid_size
+				# 	for l in range(0, self.resolution):
+				# 		y = section_y + (l+0.5)*self.grid_size
+				# 		if (self.center.x-x)**2+(self.center.y-y)**2 < self.radius**2:
+				# 			section.append(Point(x,y))
+				# sections.append(section)
+				section = SearchAreaSection(Point(section_x, section_y), fov_radius, self.resolution, self)
+				section.update() # update once to finish setting up
 				sections.append(section)
 		self.sections = sections
 	
 	def update_coverage_section(self, x_section, y_section):
 		section_index = x_section*self.sections_per_row + y_section
-		to_remove = []
+		# to_remove = []
 		try:
-			for point in self.sections[section_index]:
-				if point_is_in_fov(point):
-					to_remove.append(point)
-			for point in to_remove:
-				self.sections[section_index].remove(point)
+			# for point in self.sections[section_index]:
+			# 	if point_is_in_fov(point):
+			# 		to_remove.append(point)
+			# for point in to_remove:
+			# 	self.sections[section_index].remove(point)
+			self.sections[section_index].update()
 		except IndexError:
 			pass
 		
@@ -833,20 +904,12 @@ def draw_search_area():
 
 				glPushMatrix()
 				
-				glBegin(GL_QUADS)
-				grid_extent = search_area.grid_size/2*camera.scale
 				for section in search_area.sections:
-					for point in section:
-						(x, y) = camera.lps_to_screen(point.x, point.y)
-						glVertex2f(x+grid_extent, y-grid_extent)
-						glVertex2f(x-grid_extent, y-grid_extent)
-						glVertex2f(x-grid_extent, y+grid_extent)
-						glVertex2f(x+grid_extent, y+grid_extent)
-				glEnd()
-
+					section.draw(camera)
+					
 				glPopMatrix()
 				glDisable(GL_BLEND)
-				
+
 				end = time.time()
 
 				gl_time = gl_time*0.8 + (end-start) * 0.2
