84f7a653381b5039c64cf861536ef9dc6d0cd6a5
Matthew Reynolds <mtreynolds@uwaterloo.ca>
2018-12-03 21:06:55 -0500

    Add yapf config file

--- dependencies.sh
+++ dependencies.sh
@@ -1,8 +1,9 @@
-# NOTE: This is a file to install dependancies for travis.  Rosdep currently
-# installs most dependancies, but those that can't be added to the package.xml
-# file must be added here,  to download from apt
-sudo apt-get install python-pip
+# NOTE: This is a file to install dependencies for Travis.  Rosdep currently
+# installs most dependencies, but those that can't be added to the package.xml
+# file must be added here, to download from apt
+sudo apt install python-pip
 pip install freetype-py
 pip install overrides
 pip install doxypypy # Not needed for Travis, but needed for Doxygen
 pip install opencv-python
+pip install yapf
