76a04f6c1a391c643b86e6f7b533d7261600ece3
Matthew Reynolds <mtreynolds@uwaterloo.ca>
2018-12-03 22:46:07 -0500

    Autoformat boat_interfaces

--- boat_interfaces/tools/connect_serial.py
+++ boat_interfaces/tools/connect_serial.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
-          
-      
+
 import time
 import serial
-          
-      
+
 ser = serial.Serial(
-port='/dev/ttyUSB0',
-baudrate = 115200,
-parity=serial.PARITY_NONE,
-stopbits=serial.STOPBITS_ONE,
-bytesize=serial.EIGHTBITS,
-timeout=1
-)
-counter=0
-          
-      
+    port='/dev/ttyUSB0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1)
+counter = 0
+
 while 1:
-   x=ser.readline()
-   print x
+    x = ser.readline()
+    print x
