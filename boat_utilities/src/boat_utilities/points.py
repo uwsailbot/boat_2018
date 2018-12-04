81cafa20a77b02bf6634ec1a8ad800c8c33f5c82
Matthew Reynolds <mtreynolds@uwaterloo.ca>
2018-12-03 21:57:30 -0500

    Autoformat boat_utilities

--- boat_utilities/src/boat_utilities/points.py
+++ boat_utilities/src/boat_utilities/points.py
@@ -4,12 +4,12 @@
 def dist(a, b):
     """Determine the distance between between two points."""
     from math import sqrt
-    return sqrt((a.y - b.y)**2  + (a.x - b.x)**2)
+    return sqrt((a.y - b.y)**2 + (a.x - b.x)**2)
 
 
 def is_within_dist(a, b, tol):
     """Determine if the dist between two points is within the specified tolerance.
-    
+
     Args:
         p1: The first point to compare
         p2: The second point to compare
@@ -17,4 +17,4 @@ def is_within_dist(a, b, tol):
     Returns:
         `True` if the points are within the tolerance
     """
-    return dist(a,b) < tol
+    return dist(a, b) < tol
