6b45a162b738c2922dcf8963678619cf8c244ed2
Matt Reynolds <mtreynolds@uwaterloo.ca>
2019-04-03 00:26:28 -0700

    Add rospy<->cantp link
    Note that this has only been tested on a loopback to itself.
    Needs to be tested on real HW

---
+++ boat_interfaces/src/iso_tp/iso_tp_headers.py
@@ -0,0 +1,135 @@
+import can
+
+
+class Message(object):
+    """
+    A general message structure, containing data of an arbitrary length
+    """
+
+    def __init__(self, tx_id=0, rx_id=0, data=[]):
+        self.tx_id = tx_id
+        self.rx_id = rx_id
+        self.data = data
+        self.timestamp = None
+
+
+class SingleFrame(can.Message):
+    """
+    Single CAN frame
+
+    Each single frame contains up to 7 bytes, plus 1 PCI byte.
+
+    Schema:
+     - 0H = ISO-TP Frame Type = 0
+     - 0L = Data length, from 1 to 7 bytes
+     - 1-7 = Data
+    """
+    FRAME_TYPE = 0
+
+    def __init__(self, data=[]):
+        super(SingleFrame, self).__init__()
+
+        assert len(data) <= 7, 'Data is too large'
+
+        self.can_dlc = len(data)
+        self.data = []
+        self.data.append((SingleFrame.FRAME_TYPE << 4) | (self.can_dlc & 0x0F))
+        self.data += data
+
+
+class FirstFrame(can.Message):
+    """
+    First CAN frame in a multi-frame message
+
+    Each first frame contains exactly 6 bytes of data, plus 2 PCI bytes,
+    resulting in a total payload length of 8 bytes.
+
+    Schema:
+     - 0H = ISO-TP Frame Type = 1
+     - 0L 1H 1L = Data length, from 8 to 4095 bytes
+     - 2-7 = Data
+    """
+    FRAME_TYPE = 1
+
+    def __init__(self, data=[]):
+        super(FirstFrame, self).__init__()
+
+        assert len(data) <= 4095, 'Data is too large'
+
+        self.can_dlc = 8
+        self.data = []
+        self.data.append((FirstFrame.FRAME_TYPE << 4) | (len(data) >> 8 & 0x0F))
+        self.data.append(len(data) & 0xFF)
+        self.data += data[:6]
+
+
+class ConsecutiveFrame(can.Message):
+    """
+    Consecutive CAN frame in a multi-frame message
+
+    Each consecutive frame contains 1 to 7 bytes of data, plus 1 PCI byte.
+
+    The sequence number is 1 for the first consecutive frame in a message, and
+    increments with each subsequent consecutive frame. It overflows/wraps at 15,
+    resulting in a sequence (1, 2, 3, ... , 15, 0, 1, ... )
+
+    Schema:
+    - 0H = ISO-TP Frame Type = 2
+    - 0L = Sequence number, from 0-15
+    - 1-7 = Data
+    """
+    FRAME_TYPE = 2
+
+    def __init__(self, sequence=0, data=[]):
+        super(ConsecutiveFrame, self).__init__()
+
+        assert len(data) <= 4095, 'Data is too large'
+
+        self.sequence = sequence
+        self.data = []
+        self.data.append((ConsecutiveFrame.FRAME_TYPE << 4) | (self.sequence & 0x0F))
+
+        offset = (sequence * 7) - 1
+        self.can_dlc = min(len(data) - offset, 7) + 1
+        self.data += data[offset:offset + (self.can_dlc - 1)]
+
+
+class FlowFrame(can.Message):
+    """
+    Flow control CAN frame in a multi-frame message
+
+    Each flow frame contains exactly 0 bytes of data, plus 3 PCI bytes,
+    resulting in a total payload length of 3 bytes.
+
+    This frame is a response from the receiver after the first frame,
+    acknowledging the beginning of the message and setting the parameters for the
+    transmission of the next set of frames in the message.
+
+    Byte 1 of the PCI describes the number of frames that the sender may send
+    before it must wait for the next flow control frame to be received. If it is
+    0, then all remaining frames may be sent.
+
+    Byte 2 of the PCI describes the minimum required separation time between the
+    end of one message and the start of the next that the sender must adhere to.
+    Values of 0 to 127 correspond to milliseconds. Values of 0xF1 to 0xF9
+    correspond to 0.1-0.9 milliseconds. All other values are illegal.
+
+    Schema:
+    - 0H = ISO-TP Frame Type = 3
+    - 0L = Flow status. 0=Continue, 1=Wait, 2=Abort
+    - 1  = The number of frames that may be sent before the next flow frame
+    - 2  = The separation time between the frames
+    """
+    FRAME_TYPE = 3
+    STATUS_CONTINUE = 0
+    STATUS_WAIT = 1
+    STATUS_ABORT = 2
+
+    def __init__(self, status, num_frames, separation):
+        super(FlowFrame, self).__init__()
+
+        self.can_dlc = 3
+        self.data = []
+        self.data.append((FlowFrame.FRAME_TYPE << 4) | (status & 0x0F))
+        self.data.append(num_frames)
+        self.data.append(separation)
---
+++ boat_interfaces/src/iso_tp/iso_tp_wrapper.py
@@ -0,0 +1,259 @@
+import can
+import enum
+import time
+
+import iso_tp_headers
+
+
+def get_time_micros():
+    return time.time() * 1000000
+
+
+class IsoTPWrapper(object):
+    class ArbitrationMode(enum.Enum):
+        PRIORITY_ID = 0
+        LIFO = 1
+        FIFO = 2
+
+    class TxConfig(object):
+        def __init__(self):
+            self.msg = iso_tp_headers.Message()
+
+            # Tracking transmission
+            # yapf: disable
+            self.waiting = False                      # Is waiting for flow frame
+            self.sequence = 0                         # Sequence of last sent consecutive frame
+            self.remaining_frames = 0                 # Remaining num of frames that may be send
+            self.prev_frame_time = get_time_micros()  # Time the last frame was sent, in us
+            # yapf: enable
+
+            # Configuration
+            self.separation = 0  # Separation time between frames, in 0.1ms
+
+    class RxConfig(object):
+        def __init__(self):
+            self.msg = iso_tp_headers.Message()
+
+            # Tracking transmission
+            # yapf: disable
+            self.is_active = False     # Transmission is active
+            self.sequence = 0          # Sequence of last received consecutive frame
+            self.remaining_frames = 0  # Remaining num of frames before Flow Frame
+            # yapf: enable
+
+            # Configuration
+            self.block_size = 0  # Num of frames that may be sent
+            self.separation = 0  # Separation time between frames, in 0.1ms
+
+    def __init__(self, bus, arbitration_mode=None):
+
+        self._tx_msgs = []
+        self._rx_msgs = []
+
+        self._bus = bus
+
+        if arbitration_mode is None:
+            self._arbitration_mode = IsoTPWrapper.ArbitrationMode.PRIORITY_ID
+        else:
+            self._arbitration_mode = arbitration_mode
+
+        can.Notifier(bus, [self._read_next])
+
+        self._callbacks = []
+
+    def config_flow_frame(self, rx_id, tx_id, block_size, separation):
+        """
+        Add the specified handshake details to the wrapper's list of recognized messages
+        """
+        config = IsoTPWrapper.RxConfig()
+        config.msg.rx_id = rx_id
+        config.msg.tx_id = tx_id
+        config.block_size = block_size
+        config.separation = separation
+        self._rx_msgs.append(config)
+
+        return True
+
+    def remove_flow_frame(self, rx_id, tx_id):
+        """
+        Remove the specified message from the wrapper's list of recognized wrappers
+        """
+        for rx_msg in self._rx_msgs:
+            if rx_msg.msg.rx_id == rx_id and rx_msg.msg.tx_id == tx_id:
+                self._rx_msgs.remove(rx_msg)
+                return True
+        return False
+
+    def update(self):
+        """
+        Update all outgoing messages that are mid-transmission. Must be called regularily
+        """
+
+        # Check if outgoing messages are ready to send their next consecutive frame
+        for tx_msg in self._tx_msgs[:]:
+            if tx_msg.waiting or (get_time_micros() - tx_msg.prev_frame_time) / 100 < tx_msg.separation:
+                continue
+
+            # Update the msg data
+            tx_msg.sequence += 1
+            tx_msg.prev_frame_time = get_time_micros()
+            if tx_msg.remaining_frames == 0:
+                pass
+            elif tx_msg.remaining_frames == 1:
+                tx_msg.waiting = True
+            else:
+                tx_msg.remaining_frames -= 1
+
+            # Send the next frame
+            consec = iso_tp_headers.ConsecutiveFrame(tx_msg.sequence, tx_msg.msg.data)
+            consec.arbitration_id = tx_msg.msg.tx_id
+            self._bus.send(consec)
+
+            # If all the data has been sent, cleanup
+            if (tx_msg.sequence * 7) + 6 >= len(tx_msg.msg.data):
+                self._tx_msgs.remove(tx_msg)
+
+    def register_msg_callback(self, callback):
+        """
+        Register a callback function for when a message is fully received
+        """
+        self._callbacks.append(callback)
+
+    def send_data(self, tx_id, rx_id, data):
+        msg = iso_tp_headers.Message(tx_id=tx_id, rx_id=rx_id, data=data)
+        return self.send_msg(msg)
+
+    def send_msg(self, msg):
+        """
+        Queue a message of arbitrary length for transmission
+        """
+        print('Sending msg, TX={0}, data={1}'.format(msg.tx_id, msg.data))
+
+        # If the message is very short, send a Single Frame
+        if len(msg.data) < 8:
+            frame = iso_tp_headers.SingleFrame(msg.data)
+            frame.arbitration_id = msg.tx_id
+            return self._bus.send(frame)
+
+        # Create a new message with the data
+        config = IsoTPWrapper.TxConfig()
+        config.msg = msg
+        config.sequence = 0
+        config.waiting = True
+        self._tx_msgs.append(config)
+
+        # Send the first frame
+        frame = iso_tp_headers.FirstFrame(config.msg.data)
+        frame.arbitration_id = config.msg.tx_id
+        return self._bus.send(frame)
+
+    def _read_next(self, recv_msg):
+        """
+        INTERNAL - Read the next frame from the CANbus and handle it
+        """
+
+        # Check the type of the received frame
+        # We always ignore malformed frames
+        msg_type = recv_msg.data[0] >> 4
+
+        # If the received frame is a Single Frame, repackage the msg and notify the callbacks immediately
+        if msg_type == iso_tp_headers.SingleFrame.FRAME_TYPE:
+            msg = iso_tp_headers.Message()
+            msg.timestamp = recv_msg.timestamp
+            msg.rx_id = recv_msg.arbitration_id
+            msg.data = recv_msg.data[1:]
+
+            for callback in self._callbacks:
+                callback(msg)
+
+        # If the received frame is a First Frame, prepare for consecutive frames
+        elif msg_type == iso_tp_headers.FirstFrame.FRAME_TYPE:
+            for rx_msg in self._rx_msgs:
+                if rx_msg.msg.rx_id != recv_msg.arbitration_id:
+                    continue
+
+                # Append the new data, prepare for consecutive frames
+                rx_msg.msg.data = recv_msg.data[2:]
+                rx_msg.sequence = 0
+                rx_msg.is_active = True
+                rx_msg.remaining_frames = rx_msg.block_size
+
+                # Send flow frame
+                self._send_flow_frame(rx_msg, 0)
+
+                break
+
+        # If the received frame is a Consecutive Frame, append the data
+        elif msg_type == iso_tp_headers.ConsecutiveFrame.FRAME_TYPE:
+            for rx_msg in self._rx_msgs:
+                if rx_msg.msg.rx_id != recv_msg.arbitration_id:
+                    continue
+
+                if not rx_msg.is_active:
+                    print('Err: Got consec frame, msg inactive')
+                    return
+
+                rx_msg.sequence += 1
+                if (rx_msg.sequence & 0x0F) != (recv_msg.data[0] & 0x0F):
+                    print('Err: Got consec frame, bad sequence')
+                    return
+
+                # Append the new data
+                rx_msg.msg.data.extend(recv_msg.data[1:])
+                rx_msg.remaining_frames -= 1
+
+                # If this is the last frame in the message, trigger the callback and cleanup
+                start = (rx_msg.sequence * 7) - 1
+                if start + 7 > len(rx_msg.msg.data):
+                    rx_msg.msg.timestamp = recv_msg.timestamp
+                    for callback in self._callbacks:
+                        callback(rx_msg.msg)
+                    rx_msg.sequence = 0
+                    rx_msg.is_active = False
+                    rx_msg.msg.data = []
+
+                # If this is not the last frame of the message, but it is the last
+                # frame of the block, send the next flow frame
+                elif rx_msg.remaining_frames == 0:
+                    self._send_flow_frame(rx_msg, 0)
+                    rx_msg.remaining_frames = rx_msg.block_size
+
+                break
+
+        # If the received frame is a Flow Control Frame, continue transmission
+        elif msg_type == iso_tp_headers.FlowFrame.FRAME_TYPE:
+            for tx_msg in self._tx_msgs:
+                if tx_msg.msg.rx_id != recv_msg.arbitration_id:
+                    continue
+
+                status = recv_msg.data[0] & 0x0F
+
+                # Continue transmission
+                if status == iso_tp_headers.FlowFrame.STATUS_CONTINUE:
+                    val = recv_msg.data[2]
+                    if val >> 4 == 1:
+                        tx_msg.separation = val & 0x0F
+                    else:
+                        tx_msg.separation = val * 10
+                    tx_msg.waiting = False
+                    tx_msg.remaining_frames = recv_msg.data[1]
+                    tx_msg.prev_frame_time = get_time_micros()
+
+                # Wait for next flow control frame
+                elif status == iso_tp_headers.FlowFrame.STATUS_WAIT:
+                    tx_msg.waiting = True
+
+                # Abort transmission
+                elif status == iso_tp_headers.FlowFrame.STATUS_ABORT:
+                    self._tx_msgs.remove(tx_msg)
+
+                break
+
+        # Malformed frame
+        else:
+            pass
+
+    def _send_flow_frame(self, config, status):
+        frame = iso_tp_headers.FlowFrame(status, config.block_size, config.separation)
+        frame.arbitration_id = config.msg.tx_id
+        self._bus.send(frame)
