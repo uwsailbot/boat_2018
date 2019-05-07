af5d9204b272bc10dd1e182952ae7ea6c5c0f782
Matt Reynolds <mtreynolds@uwaterloo.ca>
2019-05-06 23:23:44 -0400

    Fix industrial_ci exclusion from linter

--- scripts/check_code_style.sh
+++ scripts/check_code_style.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Checking code style with yapf..."
-yapf --diff --recursive -e .ci_config/* . > /dev/null
+yapf --diff --recursive -e .ci_config . > /dev/null
 
 if [ $? -ne 0 ]; then
 	echo >&2 "Code does not meet style requirements! Please run yapf to lint and format the code."
